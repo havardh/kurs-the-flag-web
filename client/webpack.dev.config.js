25c6d80748cba3189081a333d133026b7a50a8dc
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2016-03-01 22:26:31 +0100

    chore: add basic config for react app with webpack

---
+++ client/webpack.dev.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('./', 'build'),
+    devtoolLineToLine: true,
+    publicPath: '/build/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react']
+        }
+      },
+      { test: /\.css$/, loaders: [ 'style', 'css' ] },
+      { test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url?limit=10000' },
+      { test: /\.scss$/, loaders: [ 'style', 'css', 'sass' ] },
+    ]
+  }
+};
