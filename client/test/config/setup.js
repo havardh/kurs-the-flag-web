ac72d8c3b687690010201ec64a4c5f5227e2a788
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2016-03-06 21:33:40 +0100

    chore: add sinon test librarry

--- client/test/config/setup.js
+++ client/test/config/setup.js
@@ -1,3 +1,9 @@
 import chai from "chai"
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
+
+chai.use(sinonChai);
+chai.should();
 
 global.expect = chai.expect;
+global.sinon = sinon;
