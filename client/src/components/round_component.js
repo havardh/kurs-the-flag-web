b818990931b99c14eb1b73204752ff1ff74af3ea
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2016-03-06 23:26:58 +0100

    Simplified sub-components in round component

--- client/src/components/round_component.js
+++ client/src/components/round_component.js
@@ -3,59 +3,38 @@ import {connect} from "react-redux";
 
 import {COLORS} from "../constants/colors";
 
-class ScoreBoard extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>Score</h2>
-        <div>Clock: <span>3:20</span></div>
-        <div>Team A: <span>100</span></div>
-        <div>Team B: <span>200</span></div>
-      </div>
-    );
-  }
-}
+const ScoreBoard = (props) => (
+  <div>
+    <h2>Score</h2>
+    <div>Clock: <span>3:20</span></div>
+    <div>Team A: <span>100</span></div>
+    <div>Team B: <span>200</span></div>
+  </div>
+);
 
-class PlayerState extends React.Component {
+const PlayerState = ({player}) => (
+  <div style={{float: "left", margin: 20 + "px"}}>
+    <h3>{player.name || "Unnamed Player"}</h3>
+    <div>Color: <span>{COLORS[player.color]}</span></div>
+  </div>
+);
 
-  render() {
-    return (
-      <div style={{float: "left", margin: 20 + "px"}}>
-        <h3>{this.props.name}</h3>
-        <div>Color: <span>{COLORS[this.props.color]}</span></div>
-      </div>
-      );
-  }
-}
+const GameState = (props) => (
+  <div>
+    {_.map(props.players, (player, i) => (
+     <PlayerState
+        key={i}
+        player={player}
+      />
+    ))}
+  </div>
+);
 
-PlayerState.defaultProps = {
-  name: "Unnamed Player"
-};
-
-class GameState extends React.Component {
-  render() {
-    const players = _.get(this.props, "players");
-    return (
-      <div>
-        {_.map(players, ({name, color}, i) => (
-          <PlayerState
-            key={i}
-            name={name}
-            color={color}
-          />
-        ))}
-      </div>
-    );
-  }
-}
-
-const Round = connect(
-  ({players}) => ({players})
-)((props) => (
+const Round = (props) => (
   <div>
     <ScoreBoard />
     <GameState players={props.players} />
   </div>
-));
+)
 
-export default Round;
+export default connect(state => state)(Round);
