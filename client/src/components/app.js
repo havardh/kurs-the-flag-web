e8893c7e7f786101db72fb6deb57b45113fcc814
Rolf Erik Lekang <me@rolflekang.com>
2016-03-29 18:32:41 +0200

    fix: Error rendering

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,14 +2,14 @@ import React, { PropTypes } from 'react';
 
 export const App = ({ errors, children }) => (
   <div>
-    <h1>The Flag</h1>
-    {errors && errors.length &&
+    {errors && errors.length > 0 &&
       <div className="errors">
         {errors.map(error =>
           <div className="error" dangerouslySetInnerHTML={{ __html: error }} />
         )}
       </div>
     }
+    <h1>The Flag</h1>
     <div>{children}</div>
   </div>
 );
