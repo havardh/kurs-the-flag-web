3479416ae55499da5a9ec1d8bf800c015cc3d2a1
Trygve Aaberge <trygve.aaberge@itera.no>
2016-03-30 18:40:20 +0200

    Listen on all interfaces

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('babel-core/register');
 var app = require('./server/app').default;
 var config = require('./config');
 
-app.listen(config.PORT, 'localhost', err => {
+app.listen(config.PORT, '0.0.0.0', err => {
   if (err) {
     console.log(err); // eslint-disable-line no-console
     return;
